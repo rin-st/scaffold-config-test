58e60746a92b08ec8fdc421ebb796df68cf2d056
Rinat <rinat@hey.com>
2025-03-07 15:37:15 +0300

    fix: update args

--- extension/packages/nextjs/scaffold.config.ts.args.mjs
+++ extension/packages/nextjs/scaffold.config.ts.args.mjs
@@ -1,10 +1,18 @@
+export const extraConfigTypeName = "ExtraConfig";
+
 export const preConfigContent = `
 // Custom variables
 const CUSTOM_API_KEY = process.env.CUSTOM_API_KEY;
+
+export type ${extraConfigTypeName} = {
+  // Random comment
+  customApiKey: string | undefined;
+}
 `;
 
 export const configOverrides = {
   targetNetworks: ["$$chains.base$$"],
   pollingInterval: 12_345,
   onlyLocalBurnerWallet: false,
+  customApiKey: "$$CUSTOM_API_KEY$$",
 };
